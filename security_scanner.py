abc34c4c185cfd8338b4d429c4cdbe8ecb81f32f
biadevs <154815546+biadevs@users.noreply.github.com>
2026-04-20 12:29:15 +0200

    Update security_scanner.py

--- security_scanner.py
+++ security_scanner.py
@@ -4,7 +4,7 @@ logs = [
     "User login: guest",
     "ALERT: Unauthorized access attempt from IP 192.168.1.50",
     "User login: user1",
-    "WARNING: Multiple failed logins from IP 10.0.0.15" 
+    "WARNING: Multiple failed logins from IP 10.0.0.15",
     "CRITICAL: System kernel modification detected!" 
 ]
 
